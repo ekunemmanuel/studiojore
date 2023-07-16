085bcf6bc100f304ffcb6055fcf98f3ad1789271
Gameznut <emmanuelapabiekun@gmail.com>
2023-07-16 09:15:52 +0100

    added the logo, optimise for seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,16 +7,60 @@ export default defineNuxtConfig({
         lang: "en",
       },
       charset: "utf-16",
-      title: "Studio Jore",
+      title: "STUDIO JORE",
       script: [
         {
           src: "./loader.js",
         },
       ],
+
       meta: [
         {
           name: "description",
-          content: "Studio Jore a brand agency where you an get the best image",
+          property: "og:description",
+          content: "STUDIO JORE is a leading design, photography, and marketing agency that offers innovative creative solutions for businesses. Elevate your brand, drive growth, and tell your unique story through captivating visuals and strategic marketing campaigns.",
+        },
+        {
+          name: "keywords",
+          content: "design agency, photography, marketing, branding, social media marketing",
+        },
+        {
+          name: "author",
+          content: "STUDIO JORE",
+        },
+        {
+          name: "robots",
+          content: "index, follow",
+        },
+        {
+          "http-equiv": "X-UA-Compatible",
+          content: "ie=edge",
+        },
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1.0",
+
+        },
+        {
+          name: "charset",
+          content: "UTF-8",
+        },
+        {
+          name: "title",
+          content: "STUDIO JORE - Design, Photography, and Marketing Agency",
+          property: "og:title",
+        },
+        {
+          property: "og:type",
+          content: "website",
+        },
+        {
+          property: "og:url",
+          content: "https://studiojore.vercel.app/",
+        },
+        {
+          property: "og:image",
+          content: "https://studiojore.vercel.app/logo-large.png",
         },
       ],
       link: [
